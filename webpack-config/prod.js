ffd350188213ef602d5ba2b809a168742bd0a327
Eathyn <eathynzakas@gmail.com>
2023-10-14 18:08:20 +0800

    fix: remove log

--- webpack-config/prod.js
+++ webpack-config/prod.js
@@ -1,6 +1,5 @@
 const { loadEnv } = require('./utils')
 loadEnv('production')
-console.log('NODE_ENV in prod.js: ', process.env.NODE_ENV)
 
 const { merge } = require('webpack-merge')
 const base = require('./base')
