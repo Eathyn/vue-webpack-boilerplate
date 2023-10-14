3ec8c740674cb83b3297232c478f7a9e18325abd
Eathyn <eathynzakas@gmail.com>
2023-10-14 18:08:28 +0800

    babel

--- webpack-config/base.js
+++ webpack-config/base.js
@@ -10,6 +10,16 @@ module.exports = {
     clean: true,
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: resolve(process.cwd(), 'public/index.html'),
