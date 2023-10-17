03deb05eff3fd1afd2a2c0f18e9d37766c09cdbe
Eathyn <eathynzakas@gmail.com>
2023-10-17 17:18:05 +0800

    Pinia

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,15 @@
 <template>
   <div>home</div>
+  <div>count: {{ count }}</div>
+  <div>double count: {{ doubleCount }}</div>
+  <button @click="increment">increment</button>
 </template>
+
+<script setup lang="ts">
+import { useCounterStore } from '@/store/counter'
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore()
+const { count, doubleCount } = storeToRefs(store)
+const { increment } = store
+</script>
